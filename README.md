726d8386c492c9e1554ab46830ed72fe1a62e043
Tom Wright <tom@inflatablecookie.com>
2023-11-10 17:46:03 +0000

    Built initial implementation
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

---
+++ README.md
@@ -0,0 +1,74 @@
+# Slingshot
+
+[![PHP from Packagist](https://img.shields.io/packagist/php-v/decodelabs/slingshot?style=flat)](https://packagist.org/packages/decodelabs/slingshot)
+[![Latest Version](https://img.shields.io/packagist/v/decodelabs/slingshot.svg?style=flat)](https://packagist.org/packages/decodelabs/slingshot)
+[![Total Downloads](https://img.shields.io/packagist/dt/decodelabs/slingshot.svg?style=flat)](https://packagist.org/packages/decodelabs/slingshot)
+[![GitHub Workflow Status](https://img.shields.io/github/actions/workflow/status/decodelabs/slingshot/integrate.yml?branch=develop)](https://github.com/decodelabs/slingshot/actions/workflows/integrate.yml)
+[![PHPStan](https://img.shields.io/badge/PHPStan-enabled-44CC11.svg?longCache=true&style=flat)](https://github.com/phpstan/phpstan)
+[![License](https://img.shields.io/packagist/l/decodelabs/slingshot?style=flat)](https://packagist.org/packages/decodelabs/slingshot)
+
+### Unified dependency injection invoker
+
+Slingshot provides a simple interface for invoking methods on objects with dependency injection.
+
+_Get news and updates on the [DecodeLabs blog](https://blog.decodelabs.com)._
+
+---
+
+## Installation
+
+Install via Composer:
+
+```bash
+composer require decodelabs/slingshot
+```
+
+## Usage
+
+Use a Slingshot instance to invoke a function with dependency injection:
+
+```php
+use DecodeLabs\Slingshot;
+
+$slingshot = new Slingshot(
+    container: $container, // Psr\Container\ContainerInterface
+    parameters: [
+        'param1' => 'hello'
+    ]
+);
+
+function test(
+    string $param1,
+    string $param2
+) {
+    return $param1 . ' '. $param2;
+}
+
+$output = $slingshot->invoke(test(...), [
+    'param2' => 'world'
+]); // hello world
+```
+
+Or instantiate an object with dependency injection:
+
+```php
+use DecodeLabs\Harvest\Context as HarvestContext;
+
+class Test {
+    public function __construct(
+        // Fetch or create a Harvest context from container
+        HarvestContext $param1,
+        string $param2
+    ) {
+        // ...
+    }
+}
+
+$testObect = $slingshot->instantiate(Test::class, [
+    'param2' => 'value'
+]);
+```
+
+## Licensing
+
+Slingshot is licensed under the MIT License. See [LICENSE](./LICENSE) for the full license text.
---
+++ src/Slingshot.php
@@ -0,0 +1,421 @@
+<?php
+
+/**
+ * @package Slingshot
+ * @license http://opensource.org/licenses/MIT
+ */
+
+declare(strict_types=1);
+
+namespace DecodeLabs;
+
+use Closure;
+use DecodeLabs\Pandora\Container as PandoraContainer;
+use Psr\Container\ContainerInterface as Container;
+use ReflectionClass;
+use ReflectionFunction;
+use ReflectionIntersectionType;
+use ReflectionNamedType;
+use ReflectionType;
+use ReflectionUnionType;
+use Throwable;
+
+class Slingshot
+{
+    public const MAX_RECURSION = 1000;
+
+    protected static int $stack = 0;
+
+    protected ?Container $container = null;
+
+    /**
+     * @var array<string, mixed>
+     */
+    protected array $parameters = [];
+
+    /**
+     * Init with container
+     *
+     * @param array<string, mixed> $parameters
+     */
+    public function __construct(
+        ?Container $container = null,
+        array $parameters = []
+    ) {
+        if (
+            $container === null &&
+            class_exists(Genesis::class)
+        ) {
+            $container = Genesis::$container;
+        }
+
+        $this->container = $container;
+        $this->parameters = $parameters;
+    }
+
+
+    /**
+     * Set container
+     *
+     * @return $this
+     */
+    public function setContainer(
+        ?Container $container
+    ): static {
+        $this->container = $container;
+        return $this;
+    }
+
+    /**
+     * Get container
+     */
+    public function getContainer(): ?Container
+    {
+        return $this->container;
+    }
+
+    /**
+     * Set parameters
+     *
+     * @param array<string, mixed> $parameters
+     * @return $this
+     */
+    public function setParameters(
+        array $parameters
+    ): static {
+        $this->parameters = $parameters;
+        return $this;
+    }
+
+    /**
+     * Add parameters
+     *
+     * @param array<string, mixed> $parameters
+     * @return $this
+     */
+    public function addParameters(
+        array $parameters
+    ): static {
+        $this->parameters = array_merge(
+            $this->parameters,
+            $parameters
+        );
+
+        return $this;
+    }
+
+    /**
+     * Has parameters
+     */
+    public function hasParameters(): bool
+    {
+        return !empty($this->parameters);
+    }
+
+    /**
+     * Get parameters
+     *
+     * @return array<string, mixed>
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+
+
+    /**
+     * Set parameter
+     *
+     * @return $this
+     */
+    public function setParameter(
+        string $name,
+        mixed $value
+    ): static {
+        $this->parameters[$name] = $value;
+        return $this;
+    }
+
+    /**
+     * Get parameter
+     */
+    public function getParameter(
+        string $name
+    ): mixed {
+        return $this->parameters[$name] ?? null;
+    }
+
+    /**
+     * Has parameter
+     */
+    public function hasParameter(
+        string $name
+    ): bool {
+        return array_key_exists($name, $this->parameters);
+    }
+
+    /**
+     * Remove parameter
+     *
+     * @return $this
+     */
+    public function removeParameter(
+        string $name
+    ): static {
+        unset($this->parameters[$name]);
+        return $this;
+    }
+
+    /**
+     * Invoke method
+     *
+     * @template T
+     * @param callable():T $function
+     * @param array<string, mixed> $parameters
+     * @return T
+     */
+    public function invoke(
+        callable $function,
+        array $parameters = []
+    ): mixed {
+        if (++self::$stack > self::MAX_RECURSION) {
+            throw Exceptional::Runtime(
+                'Maximum recursion depth reached'
+            );
+        }
+
+        if (!$function instanceof Closure) {
+            $function = Closure::fromCallable($function);
+        }
+
+        $ref = new ReflectionFunction($function);
+        $args = [];
+        $parameters = array_merge($this->parameters, $parameters);
+
+        foreach ($ref->getParameters() as $param) {
+            $type = $param->getType();
+            $name = $param->getName();
+            $value = null;
+
+            if (
+                $type instanceof ReflectionNamedType &&
+                !$type->isBuiltin()
+            ) {
+                /** @var class-string<object> $typeName */
+                $typeName = $type->getName();
+            } else {
+                $typeName = null;
+            }
+
+            if (
+                $type instanceof ReflectionUnionType ||
+                $type instanceof ReflectionIntersectionType
+            ) {
+                self::$stack--;
+                throw Exceptional::Implementation(
+                    'Union and intersection types are not supported'
+                );
+            }
+
+
+            // Parameter value
+            if (
+                array_key_exists($name, $parameters) &&
+                $this->checkType($parameters[$name], $type)
+            ) {
+                $args[$name] = $parameters[$param->getName()];
+                continue;
+            }
+
+
+            // Container value
+            if (
+                $type instanceof ReflectionNamedType &&
+                !$type->isBuiltin() &&
+                $this->container
+            ) {
+                if ($this->container instanceof PandoraContainer) {
+                    $value = $this->container->tryGet($type->getName());
+                } elseif ($this->container->has($type->getName())) {
+                    $value = $this->container->get($type->getName());
+                }
+            }
+
+            if ($value !== null) {
+                $args[$name] = $value;
+                continue;
+            }
+
+
+            // Archetype
+            if (
+                $type instanceof ReflectionNamedType &&
+                !$type->isBuiltin() &&
+                $typeName !== null &&
+                $class = Archetype::tryResolve(
+                    $typeName,
+                    [$name, null]
+                )
+            ) {
+                try {
+                    $args[$name] = $this->newInstance($class);
+                } catch (Throwable $e) {
+                    self::$stack--;
+                    throw $e;
+                }
+                continue;
+            }
+
+
+            // Default value
+            if ($param->isDefaultValueAvailable()) {
+                $args[$name] = $param->getDefaultValue();
+                continue;
+            }
+
+
+            // Null
+            if (
+                $value === null &&
+                $type !== null &&
+                $type->allowsNull()
+            ) {
+                $args[$name] = null;
+                continue;
+            }
+
+
+            // New instance
+            if (
+                $type instanceof ReflectionNamedType &&
+                !$type->isBuiltin() &&
+                $typeName !== null
+            ) {
+                try {
+                    $args[$name] = $this->newInstance($typeName);
+                } catch (Throwable $e) {
+                    self::$stack--;
+                    throw $e;
+                }
+
+                continue;
+            }
+
+            self::$stack--;
+
+            if (isset($parameters[$name])) {
+                throw Exceptional::Definition(
+                    'Parameter $' . $name . ' is not type compatible'
+                );
+            }
+
+            throw Exceptional::Definition(
+                'Unable to resolve constructor parameter $' . $param->getName()
+            );
+        }
+
+        return $function(...$args);
+    }
+
+
+    /**
+     * Check type of value
+     */
+    protected function checkType(
+        mixed $value,
+        ?ReflectionType $type
+    ): bool {
+        if (
+            $type === null ||
+            !$type instanceof ReflectionNamedType
+        ) {
+            return true;
+        }
+
+        if ($value === null) {
+            return $type->allowsNull();
+        }
+
+        if (!$type->isBuiltin()) {
+            return $value instanceof ($type->getName());
+        }
+
+        switch ($type->getName()) {
+            case 'int':
+                return is_int($value);
+
+            case 'float':
+                return is_float($value);
+
+            case 'string':
+                return is_string($value);
+
+            case 'bool':
+                return is_bool($value);
+
+            case 'array':
+                return is_array($value);
+
+            case 'object':
+                return is_object($value);
+
+            case 'iterable':
+                return is_iterable($value);
+
+            case 'callable':
+                return is_callable($value);
+
+            case 'resource':
+                return is_resource($value);
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Create new instance
+     *
+     * @template T of object
+     * @param class-string<T> $class
+     * @param array<string, mixed> $parameters
+     * @return T
+     */
+    public function newInstance(
+        string $class,
+        array $parameters = []
+    ): object {
+        if (!class_exists($class)) {
+            throw Exceptional::Logic(
+                'Class ' . $class . ' does not exist'
+            );
+        }
+
+        $ref = new ReflectionClass($class);
+        $output = $ref->newInstanceWithoutConstructor();
+
+        if ($ref->hasMethod('__construct')) {
+            $constructor = $ref->getMethod('__construct');
+
+            if (!$constructor->isPublic()) {
+                throw Exceptional::Logic(
+                    'Class ' . $class . ' has a non-public constructor'
+                );
+            }
+
+            if (!$closure = $constructor->getClosure($output)) {
+                throw Exceptional::Logic(
+                    'Unable to get closure for constructor of ' . $class
+                );
+            }
+
+            $this->invoke(
+                $closure,
+                $parameters
+            );
+        }
+
+        return $output;
+    }
+}
